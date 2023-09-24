c30c58d06f69e6941da22e5498e8047917bc6f5f
unknown <gausulislam5@gmail.com>
2023-09-25 01:29:36 +0600

    update section

---
+++ js/loop.js
@@ -0,0 +1,9 @@
+const numbers=[12,54,65,3,54];
+
+// for (const number of numbers) {
+//     console.log(number); 
+// }
+
+//for of cannot used with objects
+const bottle= {color:'yellow', price:50,isCleaned:true,capacity:1};
+
