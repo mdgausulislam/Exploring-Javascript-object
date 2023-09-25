7b19999dec189fa37d55471e3859bfe8a09d9ed8
unknown <gausulislam5@gmail.com>
2023-09-25 23:29:52 +0600

    update section

---
+++ js/passBy.js
@@ -0,0 +1,21 @@
+
+let num1 = 5;
+let num2 = 7;
+
+function multiply(a, b) {
+    a = 27;
+    const result = a * b;
+    return result;
+}
+const output = multiply(num1, num2);
+console.log(output);
+
+
+let student={name:'hh' ,partner:'bbb'};
+let student2={name:'jj' ,partner:'pp'};
+
+function makeMovie(couple1,couple2){
+    couple1.name='ononto'
+}
+console.log(student);
+makeMovie(student,student2);
